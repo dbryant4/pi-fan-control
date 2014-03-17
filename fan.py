e2dd89ae70904efef402bc65b711d9c260f489b4
root <root@raspberrypi.(none)>
2014-03-17 12:08:23 -0400

    Push graphite data using graphiteudp, improved temperate reading, loads config file relative to scripts path

--- fan.py
+++ fan.py
@@ -1,8 +1,11 @@
+#!/usr/bin/python
 import sys
+import os
 import time
 import logging
 import smbus
 import urllib2
+import graphiteudp
 
 from fancontrol import fan_control
 
@@ -18,7 +21,7 @@ except:
     print "Unable to load \"nest\" module. See README.md for URL"
 
 # Read configuration file
-cfg = Config(file('local_settings.cfg'))
+cfg = Config(file(os.path.dirname(os.path.realpath(__file__)) + '/local_settings.cfg'))
 
 # Setup logging module
 numeric_level = getattr(logging, cfg.log_level.upper(), None)
@@ -41,6 +44,8 @@ except urllib2.URLError:
     
 #print n.status['device'][n.serial]
 #print n.status['shared'][n.serial]
+print cfg.graphite_prefix.lower()
+graphiteudp.init(cfg.graphite_host, prefix = cfg.graphite_prefix.lower())
 
 bus = smbus.SMBus(cfg.smbus)
 
@@ -54,14 +59,21 @@ while True:
     hvac_fan_on = n.status['shared'][n.serial]['hvac_fan_state']
     while True:
         try:
-            current_temperature = bus.read_byte(0x48) * 1.8 + 32
+            data = bus.read_i2c_block_data(0x48, 0)
         except IOError:
             logging.debug("Error getting temperature. Retrying...")
         else:
             break
 
-    logging.debug("Temperature: %s", current_temperature)
+    msb = data[0]
+    lsb = data[1]
+    current_temperature_c = (((msb << 8) | lsb) >> 4) * 0.0625
+    current_temperature = current_temperature_c * 1.8 + 32.0
+    logging.debug("Temperature: %s F (%s C)", current_temperature, current_temperature_c)
     
+    graphiteudp.send("closet.temperature.f",current_temperature)
+    graphiteudp.send("closet.temperature.c",current_temperature_c)
+ 
     if away:
         logging.debug("Auto away enabled. Turning on fan.")
         fan.turn_on()
